2a1fa8bf589362df13bd537c906d61bce3f1e424
Edouard <laffont.edu@gmail.com>
2025-11-05 14:15:30 +0100

    Spikes and Moving platform

--- Scripts/moving_platform.gd
+++ Scripts/moving_platform.gd
@@ -1,15 +1,17 @@
-extends StaticBody2D
+extends CharacterBody2D
 
-@export var SPEED: int
+@export var SPEED: float
 @export var directionX: float
 @export var directionY: float
 
+var direction: Vector2
 
-func _ready() -> void:
-	pass 
+func _physics_process(delta: float) -> void:
+	if velocity.x != directionX and velocity.y != directionY:
+		velocity.x = move_toward(velocity.x, directionX, SPEED)
+		velocity.y = move_toward(velocity.y, directionY, SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, -directionX, SPEED)
+		velocity.y = move_toward(velocity.y, -directionY, SPEED)
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	move_and_slide()
