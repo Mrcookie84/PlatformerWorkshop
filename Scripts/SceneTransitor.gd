575cbea90f0bc3dbe2ea6394a1da2946ca87c7f3
Bard Avelia <a.bard@rubika-edu.com>
2025-11-07 16:09:25 +0100

    assert to not forget to set the transition

--- Scripts/SceneTransitor.gd
+++ Scripts/SceneTransitor.gd
@@ -6,6 +6,9 @@ signal player_transition_request(transition_request : SceneTransitionRequest)
 @export
 var transition_request : SceneTransitionRequest
 
+func _ready() -> void:
+	assert(transition_request,"transition not set")
+
 func _on_player_enter(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	if (body.is_in_group("player")):
 		player_transition_request.emit(transition_request)
