36e3c370e34b4d92a795eae9cd5aa8006f9c21a0
Edouard <laffont.edu@gmail.com>
2025-11-07 10:37:22 +0100

    Fan working

--- Scripts/up_fan.gd
+++ Scripts/up_fan.gd
@@ -3,8 +3,18 @@ extends Area2D
 @export var SPEED: float
 @export var target_direction: Vector2
 
-var target: CharacterBody2D
+var target: Player #CharacterBody2D
 
 func _on_body_entered(body: Node2D) -> void:
 	target = body
-	target.global_position = target.global_position.move_toward(target_direction, SPEED)
+	target.is_on_floor() == false
+	print(target.is_on_floor())
+	
+func _on_body_exited(body: Node2D) -> void:
+	print(target.is_on_floor())
+	target = null
+
+	
+func _physics_process(delta: float) -> void:
+	if target:
+		target.velocity += target_direction * SPEED * delta
