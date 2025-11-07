f50ca3a53d1aab987a97967d229efdb6587a6c70
Bard Avelia <a.bard@rubika-edu.com>
2025-11-07 15:51:43 +0100

    migrated to transition new system

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,16 +1,19 @@
 extends Node2D
 class_name Level
 
+@export
 var world_manager : WorldManager
+
 
 var player : Player
 
 var entry_points : Array[EntryPoint]
 
 func _ready() -> void:
-	entry_points = get_children() \
+	entry_points.assign(get_children() \
 			.map(connect_signal_transition) \
-			.filter(func(a): return a is EntryPoint)
+			.filter(func(a): return a is EntryPoint))
+	entry_points.map(func (a): print(a.name))
 
 func connect_signal_transition(n:Node) -> Node:
 	if n is not SceneTransitor:
@@ -22,6 +25,6 @@ func connect_signal_transition(n:Node) -> Node:
 	return n
 
 func do_player_spawn(transition: SceneTransitionRequest) -> void:
-	var entry_point : EntryPoint = entry_points.filter(func(a:EntryPoint): a.name == transition.future_spawn_point_id)[0]
+	var entry_point : EntryPoint = entry_points.filter(func(a:EntryPoint):return a.name == transition.future_spawn_point_id).front()
 
 	player.position = entry_point.position
--- Scripts/world_manager.gd
+++ Scripts/world_manager.gd
@@ -4,13 +4,29 @@ class_name WorldManager
 @export
 var current_level : Level
 
+@onready
+var tree: SceneTree = get_tree()
+
+@export
+var player: Player
 
 func change_level(transition_request : SceneTransitionRequest) -> void :
-	var level : Level = (ResourceLoader.load_threaded_get(transition_request.scene_to_transition) as PackedScene).instantiate()
+	var path : String = ResourceUID.uid_to_path(transition_request.scene_to_transition)
+
+	ResourceLoader.load_threaded_request(path)
+
+	while ResourceLoader.load_threaded_get_status(path) == 1 :
+		await tree.process_frame
+
+	var level : Level = ResourceLoader.load_threaded_get(path).instantiate()
+
 
 	level.world_manager = self
 	
 	remove_child(current_level)
 	add_child(level)
+
 	# make shit happens like player reposition and all
+	
+	level.player = player
 	level.do_player_spawn(transition_request)
